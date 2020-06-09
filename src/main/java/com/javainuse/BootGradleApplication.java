bb392f47dfa48f5eaac067187470fe796362fe40
TheDevopsGurus <55923896+TheDevopsGurus@users.noreply.github.com>
2020-06-08 22:14:04 -0500

    Update BootGradleApplication.java

--- src/main/java/com/javainuse/BootGradleApplication.java
+++ src/main/java/com/javainuse/BootGradleApplication.java
@@ -2,11 +2,42 @@ package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class BootGradleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootGradleApplication.class, args);
+		
+		Scanner input = new Scanner(System.in);
+
+    int firstNumber;
+    int secondNumber;
+
+    System.out.println("Write a number");
+    firstNumber = Integer.parseInt(input.nextLine());
+
+    System.out.println("Write another number");
+    secondNumber = Integer.parseInt(input.nextLine());
+
+    Addition(firstNumber, secondNumber);
+    Substratcion(firstNumber, secondNumber);
+    Multiplication(firstNumber, secondNumber);
 	}
+	
+	
+	static void Addition( int firstNumber, int secondNumber ) {
+        System.out.println("Addition = " + (firstNumber + secondNumber));
+    }
+
+
+    static void Substratcion( int firstNumber, int secondNumber ) {
+        System.out.println("Substratcion = " + (firstNumber - secondNumber));
+    }
+
+
+    static void Multiplication( int firstNumber, int secondNumber ) {
+        System.out.println("Multiplication = " + (firstNumber * secondNumber));
+    }
 }
