3bc0eac2862d08fb0bb42d3f80cfe993ff7d3d3b
TheDevopsGurus <55923896+TheDevopsGurus@users.noreply.github.com>
2020-06-08 21:19:38 -0500

    Create SimleCalculator.java

---
+++ src/main/java/com/javainuse/SimleCalculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class SimleCalculator{
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    int firstNumber;
+    int secondNumber;
+
+    System.out.println("Write a number");
+    firstNumber = Integer.parseInt(input.nextLine());
+
+    System.out.println("Write another number");
+    secondNumber = Integer.parseInt(input.nextLine());
+
+    Addition(firstNumber, secondNumber);
+    Substratcion(firstNumber, secondNumber);
+    Multiplication(firstNumber, secondNumber);
+
+  }
+
+    static void Addition( int firstNumber, int secondNumber ) {
+        System.out.println("Addition = " + (firstNumber + secondNumber));
+    }
+
+
+    static void Substratcion( int firstNumber, int secondNumber ) {
+        System.out.println("Substratcion = " + (firstNumber - secondNumber));
+    }
+
+
+    static void Multiplication( int firstNumber, int secondNumber ) {
+        System.out.println("Multiplication = " + (firstNumber * secondNumber));
+    }
+
+}
