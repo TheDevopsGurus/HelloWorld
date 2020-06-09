32eab6ba41738dbf260c283528b4154bd21a47b7
TheDevopsGurus <55923896+TheDevopsGurus@users.noreply.github.com>
2020-06-09 08:30:38 -0500

    Create App.java

---
+++ src/main/java/com/javainuse/App.java
@@ -0,0 +1,10 @@
+package com.javainuse;
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {  
+        System.out.println(new App().getGreeting());
+    }
+}
