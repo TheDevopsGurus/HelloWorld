f9472d1f2c03c992771c8ad31a01b39e377d9194
TheDevopsGurus <55923896+TheDevopsGurus@users.noreply.github.com>
2020-06-09 20:53:52 -0500

    Update App.java

--- src/main/java/com/javainuse/App.java
+++ src/main/java/com/javainuse/App.java
@@ -7,7 +7,8 @@ public class App
         return "Hello world.";
     }
 
-    public static void main(String[] args) {  
+    public static void main(String[] args) 
+    {  
         System.out.println(new App().getGreeting());
     }
        
