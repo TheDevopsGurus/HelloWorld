bd30b44ce268feb2fcc6fcfd9e7632ba98381ac7
sendmequickfast@gmail.com <sendmequickfast@gmail.com>
2021-07-25 20:28:32 -0500

    test4

--- modifyYml/hello.sh
+++
@@ -1,8 +0,0 @@
-
-#echo "hello"
-#mkdir -p modifyYml
-cmd1=`git log --format="%H" -n 1`
-
-#echo $cmd1
-git diff-tree --no-commit-id --name-only -r $cmd1  > output ; cp `cat output` ./modifyYml/ 
-#echo "$cmd2"
